15d3abdd8c8486a723949751c980e5e1e81164e5
Bhavyakumari <30631406+Bhavyakumari@users.noreply.github.com>
2017-08-14 17:19:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('counter', function(req,res) {
+    counter = counter +1;
+    res.send(counter,tosting());
+});
+
 app.get('/:articlename',function(req, res)
 //articlename=article-one
 //article[articlename]={} content will be the content of article one
