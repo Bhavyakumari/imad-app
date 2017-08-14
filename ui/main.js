ae4d7adca0cf4a8ec81094fe83278c91e7cef72d
Bhavyakumari <30631406+Bhavyakumari@users.noreply.github.com>
2017-08-14 16:37:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element= document.getElementById('main-text');
 element.innerHTML="new value";
 
 //move the element
-var element= document.getElementById('madi');
+var img= document.getElementById('madi');
 img.onclick = function()
    {
       img.style.marginleft='100px';
