db91fd2e0bdfee2cf51359071e1f21435697a0d7
Bhavyakumari <30631406+Bhavyakumari@users.noreply.github.com>
2017-08-14 15:13:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,65 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var aricle one={
+    title:'article one/bhavya kumari',
+    heading:'article one',
+    date:'aug 14,2017',
+    content: `<p>
+                      this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first  article. this is my content for my first  article.
+                  </p>
+                  <p>
+                      this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for myfirst article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article.
+                  </p>
+                  <p>
+                      this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my frist article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article. this is my content for my first article.
+                  </p>`
+};
+function createtempalate (data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmltemplate=`
+<html>
+    <head>
+         <title>
+            ${title}
+        </title>
+        <meta name="viewport" cotent="width-device-width,intial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+       
+          
+           </head>
+    <body>
+        <div class="container">
+           <div>
+               <a href="/">Home</a>
+               <hr/>
+                <h3>
+                ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                  ${content}
+              </div>
+        </div>
+        </div>
+    </body>
+    
+</html>
+`;
+               return  htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(reg,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createtemplate(articleone));
 });
 app.get('/article-two',function(reg,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
